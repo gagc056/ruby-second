cbde6060343ffc47894df5c0d98b3a99d3536747
lhscar <gagc056@gmail.com>
2019-10-21 16:58:03 +0200

    creating an example file

---
+++ example.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Common
+    def initialize(user)
+       cannot [:edit, :update], Legislation::Proposal do |proposal|
+        proposal.editable_by?(user)
+      end
+
+      cannot %i[edit update], Legislation::Proposal do |proposal|
+        proposal.editable_by?(user)
+      end
+    end
+  end
