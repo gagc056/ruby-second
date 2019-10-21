fbc457d1b8ca56dd41956bd0eac3df3b02fbc99f
lhscar <gagc056@gmail.com>
2019-10-21 16:25:51 +0200

    fixing sintax errors

--- enumerable.rb
+++ enumerable.rb
@@ -3,64 +3,64 @@
 module Enumerable
   def my_each
     i = 0
-    begin
+    while  i < self.size
       yield (self[i])
       i += 1
-    end until i < self.size
+    end
     self
   end
 
   def my_each_with_index
     index = 0
-    begin
+    while i < self.size
       yield (self[index])
       i += 1
-    end until i < self.size
+    end
     self
   end
 
   def my_select
     i = 0
     arr = []
-    begin
+    while  i < self.size
       if yield(self[i]) == true
         new_array << self[i]
       end
       i += 1
-    end until i < self.size
+    end
     arr
   end
 
   def my_all?
     i = 0
-    begin
+    while i < self.size
       if yield(self[i]) == false || yield(self[i]) == nil
         return false
       end
       i += 1
-    end until i < self.size
+    end
     true
   end
 
   def my_any?
     i = 0
-    begin
+    while i < self.size
       if yield(self[i])
         return true
       end
       i += 1
-    end until i < self.size
+    end
     false
   end
 
   def my_none
     i = 0
-    begin
+    while  i < self.size
       if yield(self[i])
         return false
       end
       i += 1
-    end until i < self.size
+    end
     true
   end
 
